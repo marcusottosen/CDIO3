e88643bcda2842393cc4cc7f3acfa4fb23bfe0a8
Victor E S Kongsbak <54668748+Kongsbak99@users.noreply.github.com>
2020-11-10 16:23:35 +0100

    Test mappe til JUnit tests lavet.
    Kode dice og spiller klassen indsat fra CDIO 2 projektet, med små redigeringer i det

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,2 +1,32 @@
-public class Dice {
+//Dice koden er taget fra CDIO1 opgaven.
+
+public class Dice
+{
+    private final int MAX = 6; //Maximum værdi
+
+    private int faceValue; //nuværende værdi
+
+    //Konstruktør
+    public dice() {
+        faceValue = 1;
+    }
+
+    //kaster terning
+    public int roll() {
+        faceValue =(int)(Math.random()*MAX)+1;
+
+        return faceValue;
+    }
+
+    //mutator
+    public void setFaceValue(int value)
+    {
+        faceValue = value;
+    }
+
+    //hent værdi
+    public int getFaceValue()
+    {
+        return faceValue;
+    }
 }
--- src/main/java/Spiller.java
+++ src/main/java/Spiller.java
@@ -1,2 +1,20 @@
+
 public class Spiller {
+    private int konto;
+
+    //Konstruktør
+    public spiller() {
+        konto = Logik.startKonto;
+    }
+
+    public int getkonto() {
+        return konto;
+    }
+
+    public void setKonto(int a) {
+        if (konto + a < 0)
+            konto = 0;
+        else
+            konto = konto + a;
+    }
 }
