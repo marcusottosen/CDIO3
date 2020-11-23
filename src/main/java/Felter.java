1189433e1fc28ff999f40d0cf49b36b686695d81
Rasmus Pedersen <rasmus.jdp@hotmail.com>
2020-11-23 12:28:33 +0100

    Rettet feltdescriptions

--- src/main/java/Felter.java
+++ src/main/java/Felter.java
@@ -23,37 +23,37 @@ public final class Felter {
     static GUI_Field[] gameBoard = {
             new GUI_Start("START", "Få 2 penge", "Du passerer nu start", Color.white, Color.black),
 
-            new GUI_Street("Burgerbaren", "1", "en bar", "1", red, Color.BLACK),
-            new GUI_Street("Pizzariaet", "1", "en bar", "1", red, Color.BLACK),
+            new GUI_Street("Burgerbaren", "1", "Burgerbaren", "1", red, Color.BLACK),
+            new GUI_Street("Pizzariaet", "1", "Pizzariaet", "1", red, Color.BLACK),
             new GUI_Chance("?", "Prøv lykken!", "Træk et kort og håb på det bedste", Color.white, Color.BLACK),
-            new GUI_Street("Slikbutikken", "1", "en bar", "1", orange, Color.BLACK),
-            new GUI_Street("Iskiosken", "1", "en bar", "1", orange, Color.BLACK),
+            new GUI_Street("Slikbutikken", "1", "Slikbutikken", "1", orange, Color.BLACK),
+            new GUI_Street("Iskiosken", "1", "Iskiosken", "1", orange, Color.BLACK),
 
             //GUI_Field.Image.Jail
             new GUI_Jail("GUI_Field.Image.Jail", "Fængsel", "Fængsel", "Tag på besøg i fængslet!", Color.white, Color.BLACK),
 
-            new GUI_Street("Museet", "2", "en bar", "1", green, Color.BLACK),
-            new GUI_Street("Biblikoteket", "2", "en bar", "1", green, Color.BLACK),
+            new GUI_Street("Museet", "2", "Museet", "2", green, Color.BLACK),
+            new GUI_Street("Biblikoteket", "2", "Biblikoteket", "2", green, Color.BLACK),
             new GUI_Chance("?", "Prøv lykken!", "Træk et kort og håb på det bedste", Color.white, Color.BLACK),
-            new GUI_Street("Skaterparken", "2", "en bar", "1", blue, Color.BLACK),
-            new GUI_Street("Swimmingpoolen", "2", "en bar", "1", blue, Color.BLACK),
+            new GUI_Street("Skaterparken", "2", "Skaterparken", "2", blue, Color.BLACK),
+            new GUI_Street("Swimmingpoolen", "2", "Swimmingpoolen", "2", blue, Color.BLACK),
 
             new GUI_Refuge("GUI_Field.Image.Ferry4", "Parkering", "Parkering", "Du får lov til at holde her helt gratis!", Color.white, Color.BLACK),
 
-            new GUI_Street("Spillehallen", "3", "en bar", "1", lightOrange, Color.BLACK),
-            new GUI_Street("Biografen", "3", "en bar", "1", lightOrange, Color.BLACK),
+            new GUI_Street("Spillehallen", "3", "Spillehallen", "3", lightOrange, Color.BLACK),
+            new GUI_Street("Biografen", "3", "Biografen", "3", lightOrange, Color.BLACK),
             new GUI_Chance("?", "Prøv lykken!", "Træk et kort og håb på det bedste", Color.white, Color.BLACK),
-            new GUI_Street("Legetøjsbutikken", "3", "en bar", "1", cyan, Color.BLACK),
-            new GUI_Street("Dyrehandlen", "3", "en bar", "1", cyan, Color.BLACK),
+            new GUI_Street("Legetøjsbutikken", "3", "Legetøjsbutikken", "3", cyan, Color.BLACK),
+            new GUI_Street("Dyrehandlen", "3", "Dyrehandlen", "3", cyan, Color.BLACK),
 
             //GUI_Field.Image.GoToJail
             new GUI_Jail("GoToJail.jpg", "Gå i fængsel", "Gå i fængsel",
                     "Du bliver sat i fængsel i 1 runde vedmindre du har et kort til at komme ud!", Color.white, Color.BLACK),
 
-            new GUI_Street("Bowlinghallen", "4", "en bar", "1", magenta, Color.BLACK),
-            new GUI_Street("Zoo", "4", "en bar", "1", magenta, Color.BLACK),
+            new GUI_Street("Bowlinghallen", "4", "Bowlinghallen", "4", magenta, Color.BLACK),
+            new GUI_Street("Zoo", "4", "Zoo", "4", magenta, Color.BLACK),
             new GUI_Chance("?", "Prøv lykken!", "Træk et kort og håb på det bedste", Color.white, Color.BLACK),
-            new GUI_Street("Vandlandet", "4", "en bar", "1", yellow, Color.BLACK),
-            new GUI_Street("Strandpromenaden", "4", "en bar", "1", yellow, Color.BLACK),
+            new GUI_Street("Vandlandet", "4", "Vandlandet", "4", yellow, Color.BLACK),
+            new GUI_Street("Strandpromenaden", "4", "Strandpromenaden", "4", yellow, Color.BLACK),
     };
 }
